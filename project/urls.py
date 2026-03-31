58a264562065903eef53b78c874d574ca12b8423
Caio Dias <seuemail@gmail.com>
2026-03-31 11:41:22 -0300

    fix: urls configuradas

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from acervo import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('livros/', views.listar_livros, name='listar_livros'),
+    path('devolver/<int:emprestimo_id>/', views.devolver_livro, name='devolver_livro'),
     path('admin/', admin.site.urls),
 ]
